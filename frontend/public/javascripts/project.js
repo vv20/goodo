4ad10469f17b8cecf019de3f6d7801df42a31ded
tombarwell <tomb@rwell.co.uk>
2018-11-03 15:20:08 +0000

    stuff & things

--- frontend/public/javascripts/project.js
+++ frontend/public/javascripts/project.js
@@ -1,9 +1,10 @@
-class Project {
-
-    constructor(name) {
-        this.name = name
-    }
-
-    
-
-}
+$(document).ready(() => {
+    // console.log("hello world");
+    $('body').on('click', '.panel-clickable[data-href]', function(){
+        if(window.location.hash) {
+            window.location.hash = $(this).data('href');
+        } else {
+            window.location.href = $(this).data('href');
+        }
+    })
+});
--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'goodO' });
+    res.render('index', { title: 'goodO' });
 });
 
 module.exports = router;
