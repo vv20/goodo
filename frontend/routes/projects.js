1be7e5f43f2382373f627150f4c9b5a340e5b7f2
tombarwell <tomb@rwell.co.uk>
2018-11-03 12:05:08 +0000

    ahhh

--- frontend/routes/projects.js
+++ frontend/routes/projects.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'goodO' });
+    res.render('projects', { title: 'Projects' });
 });
 
 module.exports = router;
