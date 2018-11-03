dc03eb2abe6d18efa72bdf977fb2c8bad9e6e467
kc67 <kc67@st-andrews.ac.uk>
2018-11-03 15:44:56 +0000

    fix project ish

--- frontend/app.js
+++ frontend/app.js
@@ -6,23 +6,25 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var projectRouter = require('./routes/project');
 var projectsRouter = require('./routes/projects');
+
 var loginRouter = require('./routes/login');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/project', projectRouter);
 app.use('/projects', projectsRouter);
 app.use('/login', loginRouter);
 
--- frontend/routes/project.js
+++ frontend/routes/project.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('project', { title: 'Project ' });
+router.get('/:id', function (req, res, next) {
+    res.render('projectPage', {title: 'Project', id: req.params.id});
 });
 
 module.exports = router;
